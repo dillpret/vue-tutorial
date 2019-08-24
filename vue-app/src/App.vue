332bb757ed307858a9cc2e792560197fde37a6ab
Dillon Pretorius <pretorius.dillon@gmail.com>
2019-08-24 15:51:44 +0200

    Basic form working

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-
+    <employee-form @add:employee="addEmployee" />
     <employee-table :employees="employees"/>
   </div>
 </template>
 
 <script>
   import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
 
   export default {
     name: 'app',
     components: {
-      EmployeeTable
+      EmployeeTable,
+      EmployeeForm,
     },
     data () {
       return {
@@ -33,6 +35,18 @@
             email: 'dinesh@piedpiper.com',
           },
         ],
+      }
+    },
+    methods: {
+      addEmployee(employee) {
+        const lastId =
+          this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+
+        this.employees = [...this.employees, employee];
       }
     }
 }
